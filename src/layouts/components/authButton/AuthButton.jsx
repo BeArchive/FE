7e6ad4052d3dc0a188cdf0033a978f57ff9c5844
Suki <tues1205@naver.com>
2026-01-04 01:47:09 +0900

    💄 Design: 로그인/로그아웃 버튼 UI 구현

---
+++ src/layouts/components/authButton/AuthButton.jsx
@@ -0,0 +1,21 @@
+import { cn } from '../../../lib/utils';
+
+const AuthButton = ({ isLoggedIn, onClick, className }) => {
+  const layoutStyles = 'flex justify-center items-center w-127 py-10 px-12 gap-10 rounded-8';
+  const transitionStyles = 'transition-colors duration-200';
+  const colorStyles = `
+    bg-primary-50 hover:bg-primary-200 active:bg-primary-200 
+    text-secondary-300 active:text-white
+  `;
+
+  return (
+    <button
+      onClick={onClick}
+      className={cn(layoutStyles, transitionStyles, colorStyles, className)}
+    >
+      <span className="font-medium text-16">{isLoggedIn ? '로그아웃' : '로그인'}</span>
+    </button>
+  );
+};
+
+export default AuthButton;
