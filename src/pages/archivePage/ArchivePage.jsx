9f43a47c32cbb7d3e07771a1a580b0e2d067b476
조윤지 <yunji8533@naver.com>
2026-01-07 00:31:20 +0900

    ✨ Feat: 아카이브 페이지 생성 및 라우팅 설정 추가

---
+++ src/pages/archivePage/ArchivePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ArchiveBoard from './components/archiveBoard/ArchiveBoard';
+
+const ArchivePage = () => {
+  return (
+    <div className="w-full h-full bg-primary-0">
+      <ArchiveBoard />
+    </div>
+  );
+};
+
+export default ArchivePage;
