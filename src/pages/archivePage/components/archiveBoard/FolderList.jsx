5d949a6fdd55c7a753f7d3e86b6cbfc493342c72
조윤지 <yunji8533@naver.com>
2026-01-07 00:20:40 +0900

    ✨ Feat: 폴더 리스트 및 빈 상태 UI 구현

---
+++ src/pages/archivePage/components/archiveBoard/FolderList.jsx
@@ -0,0 +1,26 @@
+import { cn } from '../../../../lib/utils';
+import archiveIcon from '../../../../assets/images/archive_empty_logo.svg';
+import FolderCard from '../archiveBoard/FolderCard';
+
+export default function FolderList({ folders = [] }) {
+  return (
+    <div className={cn('grid grid-cols-4 gap-120 px-35 py-56 w-full h-full content-start')}>
+      {folders.length > 0 ? (
+        folders.map((folder) => (
+          <FolderCard key={folder.id} folderId={folder.id} folderName={folder.name} />
+        ))
+      ) : (
+        <div className="col-span-4 flex flex-col gap-17 items-center w-full h-full justify-center">
+          {/* 빈 상태 아카이브*/}
+          <div
+            className="flex items-center justify-center"
+            style={{ width: '177px', height: '228px', padding: '4px 0 4.83px 0' }}
+          >
+            <img src={archiveIcon} alt="폴더가 없습니다" className="w-full h-full object-contain" />
+          </div>
+          <p className="font-medium text-24 text-secondary-500 leading-36">폴더가 없습니다.</p>
+        </div>
+      )}
+    </div>
+  );
+}
