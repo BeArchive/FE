d30a1044aaeb0e2b047732858b18e14dafcf3eea
suki186 <ksh.1205263@gmail.com>
2026-01-11 00:14:49 +0900

    💄 Design: 새로운 shdow 스타일 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,7 @@ export default {
       maxHeight: rem1000,
       boxShadow: {
         basic: 'var(--shadow-primary)',
+        sub: 'var(--shadow-second)',
       },
       colors: {
         black: 'var(--color-black)',
