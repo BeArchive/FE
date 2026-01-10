b830cc33fbf51243a6f991f74d1e669d59c92dbc
suki186 <ksh.1205263@gmail.com>
2026-01-11 00:14:23 +0900

    ✨ Feat: 이미지 버튼 컴포넌트 ImageButton 구현

---
+++ src/components/imageButton/ImageButton.jsx
@@ -0,0 +1,18 @@
+import { cn } from '../../lib/utils';
+
+const ImageButton = ({ src, alt = 'button-icon', onClick, className }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={cn(
+        'flex items-center justify-center p-0 bg-transparent border-none outline-none cursor-pointer',
+        'transition-transform duration-150 active:scale-95',
+        className,
+      )}
+    >
+      <img src={src} alt={alt} className="w-full h-full object-contain pointer-events-none" />
+    </button>
+  );
+};
+
+export default ImageButton;
