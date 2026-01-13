a07a69f84ddc2c2d5704d657cb300e088c9961c2
suki186 <ksh.1205263@gmail.com>
2026-01-13 18:27:11 +0900

    💄 Design: svg 아이콘용 IconButton 컴포넌트 구현

---
+++ src/components/iconButton/IconButton.jsx
@@ -0,0 +1,22 @@
+const THEMES = {
+  basic: {
+    icon: 'text-secondary-300 hover:text-primary-400 active:text-primary-main',
+    button: 'bg-primary-0 hover:bg-white active:bg-white',
+  },
+};
+
+const IconButton = ({ Icon, onClick, theme = 'basic', className = '' }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`
+        w-35 h-35 shrink-0 flex-row-center rounded-100 transition-all 
+        ${THEMES[theme].button} ${className}
+      `}
+    >
+      {Icon && <Icon className={`w-23 h-23 ${THEMES[theme].icon} transition-colors`} />}
+    </button>
+  );
+};
+
+export default IconButton;
