885c766d722d09e061a5db8ab38082659c21569b
suki186 <ksh.1205263@gmail.com>
2026-01-13 17:18:20 +0900

    💄 Design: 메인 Input UI 구현

--- src/components/ImageButton.jsx
+++ src/components/ImageButton.jsx
@@ -3,10 +3,11 @@ import { cn } from '../lib/utils';
 const ImageButton = ({ src, alt = 'button-icon', onClick, className }) => {
   return (
     <button
+      type="button"
       onClick={onClick}
       className={cn(
         'flex-row-center p-0 bg-transparent border-none outline-none cursor-pointer',
-        'transition-transform duration-150 active:scale-95',
+        'transition-transform duration-150 active:scale-95 shrink-0',
         className,
       )}
     >
--- src/components/MainInput.jsx
+++ src/components/MainInput.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import ImageButton from './ImageButton';
+import LINK from '@assets/icons/link_icon.svg';
+import SEND from '@assets/icons/send_icon.svg';
 
 const MainInput = () => {
-  return <div className="w-1064 h-94 bg-black">MainInput</div>;
+  return (
+    <div className="flex-row-center shrink-0 w-1064 h-[94px] bg-white rounded-100 px-50 shadow-sub">
+      <input
+        placeholder="새로운 30초 광고 영상을 기획하고 싶은데?"
+        className="flex-1 bg-transparent outline-none h-36 text-24 font-medium text-secondary-500 mr-50 placeholder:text-gray-400"
+      />
+      <div className="flex-row-center justify-between w-108 h-35 shrink-0">
+        <ImageButton src={LINK} className="w-35 h-35" />
+        <ImageButton src={SEND} className="w-35 h-35" />
+      </div>
+    </div>
+  );
 };
 
 export default MainInput;
