8612c7d699a7218e0d6b64246bfe9e20122fa216
조윤지 <yunji8533@naver.com>
2026-01-15 15:24:06 +0900

    💄 Design: 아카이브 보드 폴더 내부 헤더 UI 구현

---
+++ src/pages/archiveFolderPage/components/FolderHeader.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import UploadButton from './UploadButton';
+
+export default function FolderHeader({ folderName, hasNotes, onUploadClick }) {
+  return (
+    <div className="relative w-full">
+      <p className="absolute left-[41px] top-[41px] font-medium text-[20px] leading-[24px] text-secondary-500">
+        아카이브 보드 · {folderName}
+      </p>
+      {/* 구분선 */}
+      <div className="absolute left-[20px] right-[20px] top-[83px] h-[1px] bg-primary-50" />
+      {/* 업로드 버튼 (노트가 있을 때만 표시) */}
+      {hasNotes && (
+        <div className="absolute right-[50px] top-[35px]">
+          <UploadButton onClick={onUploadClick} />
+        </div>
+      )}
+    </div>
+  );
+}
