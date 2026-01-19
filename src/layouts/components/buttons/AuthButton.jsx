8d6908575861b7b4053cf53f7ed983db49a2573f
Suki <tues1205@naver.com>
2026-01-20 01:53:41 +0900

    🐛 Fix: className 기본값 지정으로 경고 밑줄 해결

--- src/layouts/components/buttons/AuthButton.jsx
+++ src/layouts/components/buttons/AuthButton.jsx
@@ -1,6 +1,6 @@
 import { cn } from '../../../lib/utils';
 
-const AuthButton = ({ isLoggedIn, onClick, className }) => {
+const AuthButton = ({ isLoggedIn, onClick, className = '' }) => {
   const layoutStyles = 'flex-row-center w-127 py-10 px-12 gap-10 rounded-8 active:scale-95';
   const transitionStyles = 'transition-colors duration-200';
   const colorStyles = `
